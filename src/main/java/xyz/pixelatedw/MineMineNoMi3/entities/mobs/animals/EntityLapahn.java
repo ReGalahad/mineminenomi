8f7c4d6123eccd1704ead7a44e85f931da0cbac3
Wynd <wyndmaster@gmail.com>
2019-07-10 16:41:13 +0300

    Implemented some AI for the Lapahn

--- src/main/java/xyz/pixelatedw/MineMineNoMi3/entities/mobs/animals/EntityLapahn.java
+++ src/main/java/xyz/pixelatedw/MineMineNoMi3/entities/mobs/animals/EntityLapahn.java
@@ -1,5 +1,6 @@
 package xyz.pixelatedw.MineMineNoMi3.entities.mobs.animals;
 
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.entity.ai.EntityAIAttackOnCollide;
 import net.minecraft.entity.ai.EntityAIHurtByTarget;
@@ -8,24 +9,30 @@ import net.minecraft.entity.ai.EntityAIMoveTowardsRestriction;
 import net.minecraft.entity.ai.EntityAINearestAttackableTarget;
 import net.minecraft.entity.ai.EntityAISwimming;
 import net.minecraft.entity.ai.EntityAIWander;
-import net.minecraft.entity.monster.EntityMob;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.DamageSource;
 import net.minecraft.world.World;
+import xyz.pixelatedw.MineMineNoMi3.api.WyHelper;
+import xyz.pixelatedw.MineMineNoMi3.api.math.WyMathHelper;
 import xyz.pixelatedw.MineMineNoMi3.api.network.WyNetworkHelper;
+import xyz.pixelatedw.MineMineNoMi3.entities.mobs.EntityNewMob;
 import xyz.pixelatedw.MineMineNoMi3.entities.mobs.INBTEntity;
+import xyz.pixelatedw.MineMineNoMi3.entities.mobs.ai.abilities.lapahn.EntityAILapahnJump;
 import xyz.pixelatedw.MineMineNoMi3.entities.mobs.bandits.BanditData;
 import xyz.pixelatedw.MineMineNoMi3.entities.mobs.pirates.PirateData;
 import xyz.pixelatedw.MineMineNoMi3.packets.PacketEntityNBTSync;
 
-public class EntityLapahn extends EntityMob implements INBTEntity
+public class EntityLapahn extends EntityNewMob implements INBTEntity
 {
 
 	public EntityLapahn(World world)
 	{
 		super(world);
+		this.setSize(0.8F, 2.5F);
 		this.tasks.addTask(0, new EntityAISwimming(this));
 		this.tasks.addTask(0, new EntityAIAttackOnCollide(this, 1.0D, false));
+		this.tasks.addTask(0, new EntityAILapahnJump(this));
 		this.tasks.addTask(1, new EntityAILookIdle(this));
 		this.tasks.addTask(2, new EntityAIMoveTowardsRestriction(this, 1.0D));
 		this.tasks.addTask(3, new EntityAIWander(this, 1.0D));
@@ -95,5 +102,30 @@ public class EntityLapahn extends EntityMob implements INBTEntity
 	protected boolean canDespawn()
 	{
 		return false;
+	}
+	
+	@Override
+	public void fall(float distance) 
+	{
+		if(distance > 5)
+		{
+			if(this.worldObj.isRemote)
+			{
+				for (int i = 0; i < 256; i++)
+				{
+					double posX = this.posX + WyMathHelper.randomWithRange(-5, 5) + WyMathHelper.randomDouble();
+					double posZ = this.posZ + WyMathHelper.randomWithRange(-5, 5) + WyMathHelper.randomDouble();
+				
+					this.worldObj.spawnParticle("explode", posX, this.posY + 0.5, posZ, 0, 0.1, 0);
+					this.worldObj.spawnParticle("smoke", posX, this.posY + 0.5, posZ, 0, 0.1, 0);		            
+				}
+			}
+			
+			for(EntityLivingBase entity : WyHelper.getEntitiesNear(this, 5))
+			{
+				entity.attackEntityFrom(DamageSource.causeMobDamage(this), 6);			
+				entity.motionY = 0.5;
+			}
+		}
 	}
 }
