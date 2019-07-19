e13e3d808b8a9da8f8b7d949682531a43e3e9777
Wynd <wyndmaster@gmail.com>
2019-07-19 17:15:37 +0300

    Fixed the large ships spawners and chests

--- src/main/java/xyz/pixelatedw/MineMineNoMi3/world/structures/StructureLargeShip.java
+++ src/main/java/xyz/pixelatedw/MineMineNoMi3/world/structures/StructureLargeShip.java
@@ -5,10 +5,8 @@ import net.minecraft.item.Item;
 import net.minecraft.tileentity.TileEntityChest;
 import net.minecraft.world.World;
 import net.minecraft.world.biome.BiomeGenBase;
-import xyz.pixelatedw.MineMineNoMi3.ID;
 import xyz.pixelatedw.MineMineNoMi3.api.Schematic;
 import xyz.pixelatedw.MineMineNoMi3.api.WySchematicHelper;
-import xyz.pixelatedw.MineMineNoMi3.blocks.tileentities.TileEntityCustomSpawner;
 import xyz.pixelatedw.MineMineNoMi3.lists.ListMisc;
 import xyz.pixelatedw.MineMineNoMi3.world.MainWorldGen;
 
@@ -30,63 +28,48 @@ public class StructureLargeShip extends Structure
 	
 	public static void populate(int posX, int posY, int posZ, World world, String faction)
 	{
+		Item swordToSpawn;
 		String trashWithSword;
 		String trashWithGun;
 		String boss;
-		int x1 = 0, x2 = 0, x3 = 0;
 		
-		Item swordToSpawn;
 		if(faction.equals("marineLargeShip"))
 		{
-			trashWithSword = ID.PROJECT_ID + ".Marine with Sword";
-			trashWithGun = ID.PROJECT_ID + ".Marine with Gun";
-			boss = ID.PROJECT_ID + ".Marine Captain";
+			trashWithSword = "Marine with Sword";
+			trashWithGun = "Marine with Gun";
+			boss = "Marine Captain";
 			swordToSpawn = ListMisc.MarineSword;
-			x1 = 13;
-			x2 = 6;
-			x3 = 10;
 		}
 		else
 		{
-			trashWithSword = ID.PROJECT_ID + ".Pirate with Sword";
-			trashWithGun = ID.PROJECT_ID + ".Pirate with Gun";	
-			boss = ID.PROJECT_ID + ".Pirate Captain";
+			trashWithSword = "Pirate with Sword";
+			trashWithGun = "Pirate with Gun";	
+			boss = "Pirate Captain";
 			swordToSpawn = ListMisc.PirateCutlass;
-			x1 = 16;
-			x2 = 9;
-			x3 = 13;
 		}
-			
+		
 		// Spawners
-		TileEntityCustomSpawner spawnTrash01 = new TileEntityCustomSpawner().setSpawnerMob(trashWithSword).setSpawnerLimit(5);
-		TileEntityCustomSpawner spawnTrash02 = new TileEntityCustomSpawner().setSpawnerMob(trashWithGun).setSpawnerLimit(5);
-		TileEntityCustomSpawner spawnTrash03 = new TileEntityCustomSpawner().setSpawnerMob(trashWithSword).setSpawnerLimit(5);
-		TileEntityCustomSpawner spawnTrash04 = new TileEntityCustomSpawner().setSpawnerMob(trashWithGun).setSpawnerLimit(5);
-		TileEntityCustomSpawner spawnTrash05 = new TileEntityCustomSpawner().setSpawnerMob(trashWithSword).setSpawnerLimit(5);
-		TileEntityCustomSpawner spawnBoss = new TileEntityCustomSpawner().setSpawnerMob(boss).setSpawnerLimit(1);
+		int[][] trash01SpawnerPositions = new int[][]
+		{
+			{posX + 10, posY + 6, posZ + 27}, {posX + 10, posY + 12, posZ + 13}, {posX + 8, posY + 12, posZ + 42}
+		};	
+		addSpawnerTileEntity(world, trash01SpawnerPositions, trashWithSword, 4, 6);
 		
-		world.setBlock(posX + 10, posY + 2, posZ + 27, ListMisc.CustomSpawner );
-		world.setTileEntity(posX + 10, posY + 2, posZ + 27, spawnTrash01);
+		int[][] trash02SpawnerPositions = new int[][]
+		{
+			{posX + 10, posY + 6, posZ + 20}, {posX + 10, posY + 12, posZ + 10}
+		};	
+		addSpawnerTileEntity(world, trash02SpawnerPositions, trashWithGun, 3, 5);
 		
-		world.setBlock(posX + 14, posY + 2, posZ + 32, ListMisc.CustomSpawner );
-		world.setTileEntity(posX + 14, posY + 2, posZ + 32, spawnTrash02);
+		int[][] bossSpawnerPositions = new int[][]
+		{
+			{posX + 12, posY + 12, posZ + 42}
+		};	
+		addSpawnerTileEntity(world, bossSpawnerPositions, boss, 1, 0);
 		
-		world.setBlock(posX + 12, posY + 8, posZ + 17, ListMisc.CustomSpawner );
-		world.setTileEntity(posX + 12, posY + 8, posZ + 17, spawnTrash03);
-		
-		world.setBlock(posX + 12, posY + 8, posZ + 11, ListMisc.CustomSpawner );
-		world.setTileEntity(posX + 12, posY + 8, posZ + 11, spawnTrash04);
-		
-		world.setBlock(posX + 14, posY + 8, posZ + 42, ListMisc.CustomSpawner);
-		world.setTileEntity(posX + 14, posY + 8, posZ + 42, spawnTrash05);
-		
-		world.setBlock(posX + 8, posY + 8, posZ + 42, ListMisc.CustomSpawner );
-		world.setTileEntity(posX + 8, posY + 8, posZ + 42, spawnBoss);
-		
-		
-		// Chests	
+		// Chests		
 		TileEntityChest combatChest = new TileEntityChest();
-		world.setTileEntity(posX + x1, posY + 2, posZ + 39, combatChest);
+		world.setTileEntity(posX + 6, posY + 6, posZ + 39, combatChest);
 		
 		addChestLoot(world, combatChest, 100, ListMisc.Bullets, 5, 10);
 		addChestLoot(world, combatChest, 100, ListMisc.Bullets, 1, 5);
@@ -99,7 +82,7 @@ public class StructureLargeShip extends Structure
 		addChestLoot(world, combatChest, 5, ListMisc.Box1, 1, 0);
 
 		TileEntityChest foodChest = new TileEntityChest();
-		world.setTileEntity(posX + x2, posY + 2, posZ + 39, foodChest);
+		world.setTileEntity(posX + 13, posY + 6, posZ + 39, foodChest);
 		
 		addChestLoot(world, foodChest, 100, Items.bread, 1, 3);
 		addChestLoot(world, foodChest, 100, Items.bread, 1, 2);
@@ -122,17 +105,18 @@ public class StructureLargeShip extends Structure
 			addChestLoot(world, foodChest, 10, Items.cooked_chicken, 1, 2);
 		addChestLoot(world, foodChest, 10, ListMisc.Cola, 0, 5);
 
-		TileEntityChest specialLootChest = new TileEntityChest();
-		world.setTileEntity(posX + x3, posY + 2, posZ + 42, specialLootChest);
+		TileEntityChest specialChest = new TileEntityChest();
+		world.setTileEntity(posX + 9, posY + 6, posZ + 42, specialChest);
+
+		addChestLoot(world, specialChest, 75, ListMisc.BellyPouch, 1, 0);
+		addChestLoot(world, specialChest, 70, ListMisc.KairosekiBullets, 5, 10);
+		addChestLoot(world, specialChest, 50, Items.boat, 1, 0);
+		addChestLoot(world, specialChest, 35, ListMisc.BellyPouch, 1, 0);
+		addChestLoot(world, specialChest, 25, ListMisc.BlackMetal, 1, 2);
+		addChestLoot(world, specialChest, 20, ListMisc.Box1, 1, 0);
+		addChestLoot(world, specialChest, 15, ListMisc.Box2, 1, 0);
+		addChestLoot(world, specialChest, 10, ListMisc.UltraCola, 1, 0);
+		addChestLoot(world, specialChest, 10, ListMisc.Box3, 1, 0);
 		
-		addChestLoot(world, foodChest, 75, ListMisc.BellyPouch, 1, 0);
-		addChestLoot(world, foodChest, 70, ListMisc.KairosekiBullets, 5, 10);
-		addChestLoot(world, foodChest, 50, Items.boat, 1, 0);
-		addChestLoot(world, foodChest, 35, ListMisc.BellyPouch, 1, 0);
-		addChestLoot(world, foodChest, 25, ListMisc.BlackMetal, 1, 2);
-		addChestLoot(world, foodChest, 20, ListMisc.Box1, 1, 0);
-		addChestLoot(world, foodChest, 15, ListMisc.Box2, 1, 0);
-		addChestLoot(world, foodChest, 10, ListMisc.UltraCola, 1, 0);
-		addChestLoot(world, foodChest, 10, ListMisc.Box3, 1, 0);
 	}
 }
