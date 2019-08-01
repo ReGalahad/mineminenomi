25d932452a315cb82d1669a7b528d711fdabf6e2
Wynd <wyndmaster@gmail.com>
2019-08-01 11:52:00 +0300

    Updated the player rank and latest version checks with database calls

--- src/main/java/xyz/pixelatedw/MineMineNoMi3/Values.java
+++ src/main/java/xyz/pixelatedw/MineMineNoMi3/Values.java
@@ -4,8 +4,14 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
+import org.apache.http.client.HttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+import com.google.gson.Gson;
+
 import net.minecraft.block.Block;
 import net.minecraft.item.Item;
+import xyz.pixelatedw.MineMineNoMi3.api.debug.WyDebug;
 import xyz.pixelatedw.MineMineNoMi3.entities.mobs.misc.EntityDojoSensei;
 import xyz.pixelatedw.MineMineNoMi3.items.AkumaNoMi;
 import xyz.pixelatedw.MineMineNoMi3.lists.ListMisc;
@@ -25,6 +31,18 @@ public class Values
 	public static final long MAX_BOUNTY = 100000000000L;
 	public static final int MAX_CREW = 50;
 	public static final int MAX_ACTIVITIES = 4;
+	
+	// Network related stuff
+	public static String urlConnection;
+	public static HttpClient httpClient = HttpClientBuilder.create().build();
+	public static Gson gson = new Gson();
+	static
+	{
+		if (WyDebug.isDebug())
+			urlConnection = "http://localhost/mmnm-webserver/api";
+		else
+			urlConnection = "http://pixelatedw.xyz/api";
+	}
 	
 	public static String RESOURCES_FOLDER;
 	
