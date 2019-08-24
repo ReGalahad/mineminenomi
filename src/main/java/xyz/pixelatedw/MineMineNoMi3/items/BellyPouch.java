cebd07eb77b96a4e24a782a66016fc84f94cade2
Wynd <wyndmaster@gmail.com>
2019-08-24 18:51:48 +0300

    New command for sharing/storing belly in pouches

--- src/main/java/xyz/pixelatedw/MineMineNoMi3/items/BellyPouch.java
+++ src/main/java/xyz/pixelatedw/MineMineNoMi3/items/BellyPouch.java
@@ -14,35 +14,37 @@ public class BellyPouch extends Item
 {
 	public BellyPouch()
 	{
-		
-	} 
+
+	}
 
 	@Override
 	public ItemStack onItemRightClick(ItemStack itemStack, World world, EntityPlayer player)
 	{
 		ExtendedEntityData props = ExtendedEntityData.get(player);
 
-		if(!world.isRemote)
-		{			
-			int amount = (int) WyMathHelper.randomWithRange(5, 100);		
+		if (!world.isRemote)
+		{
+			int amount = 0;
 			
-			if(props.getBelly() <= Values.MAX_GENERAL - amount)
+			if(!itemStack.hasTagCompound())
+				amount = (int) WyMathHelper.randomWithRange(5, 100);
+			else
+				amount = itemStack.getTagCompound().getInteger("belly");
+
+			if (props.getBelly() <= Values.MAX_GENERAL - amount)
 			{
 				props.alterBelly(amount);
-				WyHelper.sendMsgToPlayer(player, "You've obtained " + amount + " belly !");				
+				WyHelper.sendMsgToPlayer(player, "You've obtained " + amount + " belly !");
 			}
 			else
-				props.setBelly(Values.MAX_GENERAL);	
-			
-	    	if(!player.capabilities.isCreativeMode)
-	    		WyTelemetry.addMiscStat("bellyEarnedFromPouches", "Belly Earned From Pouches", amount);
-	    	
-	    	--itemStack.stackSize;		
+				props.setBelly(Values.MAX_GENERAL);
+
+			if (!player.capabilities.isCreativeMode)
+				WyTelemetry.addMiscStat("bellyEarnedFromPouches", "Belly Earned From Pouches", amount);
+
+			--itemStack.stackSize;
 		}
-		
-		//WyNetworkHelper.sendToServer(new PacketSync(props));
 
 		return itemStack;
 	}
-
 }
