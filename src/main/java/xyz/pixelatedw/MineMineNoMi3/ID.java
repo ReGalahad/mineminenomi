4532a2b8ca741c97db75c68c3ad736f02f031913
Wynd <wyndmaster@gmail.com>
2019-10-03 22:31:39 +0300

    Fixed some random crashes from suna and goro, hopefully

--- src/main/java/xyz/pixelatedw/MineMineNoMi3/ID.java
+++ src/main/java/xyz/pixelatedw/MineMineNoMi3/ID.java
@@ -77,7 +77,7 @@ public class ID
 	
 	PROJECT_ID = "mineminenomi",
 	PROJECT_NAME = "Mine Mine no Mi",
-	PROJECT_VERSION = "0.6.0b",
+	PROJECT_VERSION = "0.6.0",
 	PROJECT_MCVERSION = "1.7.10",
 
 	PARTICLEFX_DOKUGOMU = "dokuGumo",
