db1e3e160225a9aad8d93615fc09ddba0ee048fe
Wynd <wyndmaster@gmail.com>
2020-03-24 23:27:06 +0200

    Various new events for Magu users

--- src/main/java/xyz/pixelatedw/mineminenomi/events/abilities/MaguPassiveEvents.java
+++ src/main/java/xyz/pixelatedw/mineminenomi/events/abilities/MaguPassiveEvents.java
@@ -1,7 +1,17 @@
 package xyz.pixelatedw.mineminenomi.events.abilities;
 
+import com.mojang.blaze3d.platform.GlStateManager;
+
+import net.minecraft.client.Minecraft;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.tags.FluidTags;
+import net.minecraft.util.DamageSource;
 import net.minecraft.util.math.Vec3d;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.client.event.EntityViewRenderEvent;
+import net.minecraftforge.client.event.RenderBlockOverlayEvent;
+import net.minecraftforge.event.entity.living.LivingAttackEvent;
 import net.minecraftforge.event.entity.living.LivingEvent.LivingUpdateEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -23,13 +33,57 @@ public class MaguPassiveEvents
 		
 		PlayerEntity player = (PlayerEntity) event.getEntityLiving();
 		IDevilFruit devilFruitProps = DevilFruitCapability.get(player);
-		IAbilityData abilityProps = AbilityDataCapability.get(player);
 		
 		if (!devilFruitProps.getDevilFruit().equals("magu_magu"))
 			return;
 
-		if (player.isInLava())
-			player.setMotionMultiplier(player.getBlockState(), new Vec3d(player.getMotion().getX() + 25, player.getMotion().getY() + 10, player.getMotion().getZ() + 25));
+		Vec3d vec3d = player.getMotion();
+		IAbilityData abilityProps = AbilityDataCapability.get(player);
+		
+		if (player.isInLava() && !player.abilities.isFlying)
+			player.setMotion(vec3d.mul(1.9F, 1.2F, 1.9F));
+	}
+	
+	@SubscribeEvent
+	public static void onEntityAttackEvent(LivingAttackEvent event)
+	{
+		if (!(event.getEntityLiving() instanceof PlayerEntity))
+			return;	
+		
+		PlayerEntity player = (PlayerEntity) event.getEntityLiving();
+		IDevilFruit devilFruitProps = DevilFruitCapability.get(player);
+		DamageSource damageSource = event.getSource();
+
+		if(devilFruitProps.getDevilFruit().equalsIgnoreCase("magu_magu") && (damageSource.equals(DamageSource.IN_FIRE) || damageSource.equals(DamageSource.ON_FIRE) || damageSource.equals(DamageSource.LAVA)))
+		{
+			player.extinguish();
+			event.setCanceled(true);
+		}	
 	}
 
+	@OnlyIn(Dist.CLIENT)
+	@SubscribeEvent
+	public static void onEntityBurning(RenderBlockOverlayEvent event)
+	{
+		PlayerEntity player = Minecraft.getInstance().player;
+
+		if(player.isBurning() && player.areEyesInFluid(FluidTags.LAVA))
+		{
+			event.setCanceled(true);
+		}
+	}
+	
+	@OnlyIn(Dist.CLIENT)
+	@SubscribeEvent
+	public static void onEntityInLava(EntityViewRenderEvent.FogDensity event)
+	{
+		PlayerEntity player = Minecraft.getInstance().player;
+		
+		if(player.areEyesInFluid(FluidTags.LAVA))
+		{
+			event.setCanceled(true);
+			GlStateManager.fogMode(GlStateManager.FogMode.EXP);
+			event.setDensity(0.1F);
+		}
+	}
 }
