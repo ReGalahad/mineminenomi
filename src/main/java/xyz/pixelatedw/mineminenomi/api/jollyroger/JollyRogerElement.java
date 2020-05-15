558eb5e95440f7e7bd40f4aa47ce32e97599eefe
Wynd <wyndmaster@gmail.com>
2020-05-15 12:37:19 +0300

    Fully customizable Jolly Roger done

--- src/main/java/xyz/pixelatedw/mineminenomi/api/jollyroger/JollyRogerElement.java
+++ src/main/java/xyz/pixelatedw/mineminenomi/api/jollyroger/JollyRogerElement.java
@@ -1,6 +1,8 @@
 package xyz.pixelatedw.mineminenomi.api.jollyroger;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.ResourceLocation;
@@ -11,72 +13,81 @@ public class JollyRogerElement extends ForgeRegistryEntry<JollyRogerElement>
 	private boolean canBeColored = false;
 	private String color = "#FFFFFF";
 	private ResourceLocation texture;
-	private ICanUse canUseCheck = (player) -> { return true; };
+	private List<ICanUse> canUseChecks = new ArrayList<ICanUse>();
 	private LayerType type;
-	
+
 	public JollyRogerElement(LayerType type)
 	{
 		this.type = type;
 	}
-	
+
 	public boolean canBeColored()
 	{
 		return this.canBeColored;
 	}
-	
+
 	public JollyRogerElement setCanBeColored()
 	{
 		this.canBeColored = true;
 		return this;
 	}
-	
-	public String getColor() 
+
+	public String getColor()
 	{
 		return this.color;
 	}
-	
+
 	public JollyRogerElement setColor(String color)
 	{
 		this.color = color;
 		return this;
 	}
-	
+
 	public ResourceLocation getTexture()
 	{
 		return this.texture;
 	}
-	
+
 	public JollyRogerElement setTexture(ResourceLocation texture)
 	{
 		this.texture = texture;
 		return this;
 	}
-	
-	public JollyRogerElement setUseCheck(ICanUse check)
+
+	public JollyRogerElement addUseCheck(ICanUse check)
 	{
-		this.canUseCheck = check;
+		this.canUseChecks.add(check);
 		return this;
 	}
-	
+
 	public boolean canUse(PlayerEntity player)
 	{
-		return this.canUseCheck.canUse(player);
+		boolean flag = true;
+
+		for (ICanUse check : this.canUseChecks)
+		{
+			if (!check.canUse(player))
+			{
+				flag = false;
+				break;
+			}
+		}
+
+		return flag;
 	}
-	
+
 	public LayerType getLayerType()
 	{
 		return this.type;
 	}
-	
+
 	public interface ICanUse extends Serializable
 	{
 		boolean canUse(PlayerEntity player);
 	}
-	
+
 	public enum LayerType
 	{
-		BASE,
-		BACKGROUND,
-		DETAIL
+		BASE, BACKGROUND, DETAIL
 	}
 }
--- src/main/java/xyz/pixelatedw/mineminenomi/screens/JollyRogerCreatorScreen.java
+++ src/main/java/xyz/pixelatedw/mineminenomi/screens/JollyRogerCreatorScreen.java
@@ -1,6 +1,8 @@
 package xyz.pixelatedw.mineminenomi.screens;
 
 import java.awt.Color;
+import java.util.Collection;
+import java.util.List;
 import java.util.stream.Collectors;
 
 import org.lwjgl.opengl.GL11;
@@ -17,8 +19,8 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.text.StringTextComponent;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.fml.client.config.GuiUtils;
-import xyz.pixelatedw.mineminenomi.api.ModRegistries;
 import xyz.pixelatedw.mineminenomi.api.jollyroger.JollyRoger;
 import xyz.pixelatedw.mineminenomi.api.jollyroger.JollyRogerElement;
 import xyz.pixelatedw.mineminenomi.api.jollyroger.JollyRogerElement.LayerType;
@@ -34,14 +36,25 @@ public class JollyRogerCreatorScreen extends Screen
 	private PlayerEntity player;
 	private JollyRoger jollyRoger;
 	private Widget selectedButton;
-	private LayerType layerType;
-	private int layerIndex;
+	private JollyRogerElement selectedElement;
+	private LayerType layerType = LayerType.BASE;
+	
+	private int currentIndex;
+	private Collection<RegistryObject<JollyRogerElement>> allElements;
+	private List<RegistryObject<JollyRogerElement>> allBases;
+	private List<RegistryObject<JollyRogerElement>> allBackgrounds;
+	private List<RegistryObject<JollyRogerElement>> allDetails;
 	
 	public JollyRogerCreatorScreen()
 	{
 		super(new StringTextComponent(""));
 		this.player = Minecraft.getInstance().player;
 		this.jollyRoger = new JollyRoger();
+		
+		this.allElements = ModJollyRogers.JOLLY_ROGER_ELEMENTS.getEntries();
+		this.allBases = this.getTotalElementsForType(player, LayerType.BASE);
+		this.allBackgrounds = this.getTotalElementsForType(player, LayerType.BACKGROUND);
+		this.allDetails = this.getTotalElementsForType(player, LayerType.DETAIL);		
 	}
 	
 	@Override
@@ -106,7 +119,7 @@ public class JollyRogerCreatorScreen extends Screen
 			// Drawing the details	
 			for(JollyRogerElement element : this.jollyRoger.getDetails())
 			{
-				int i = 3;
+				int i = 8;
 				if(element != null && element.getTexture() != null)
 				{
 					if(element.canBeColored())
@@ -121,18 +134,18 @@ public class JollyRogerCreatorScreen extends Screen
 					Minecraft.getInstance().getTextureManager().bindTexture(element.getTexture());		
 					GuiUtils.drawTexturedModalRect(0, 0, 0, 0, 256, 256, i);
 				}		
-				i++;
+				i--;
 			}
 			
 		}
 		GL11.glPopMatrix();
 		
-		String text = 0 + " / " + this.getTotalElementsForType(LayerType.BASE);	
+		String text = (this.currentIndex + 1) + " / " + this.allBases.size();	
 		if(this.layerType == LayerType.BACKGROUND)
-			text = 0 + " / " + this.getTotalElementsForType(LayerType.BACKGROUND);
+			text = (this.currentIndex + 1) + " / " + this.allBackgrounds.size();
 		else if(this.layerType == LayerType.DETAIL)
-			text = 0 + " / " + this.getTotalElementsForType(LayerType.DETAIL);
-		WyHelper.drawStringWithBorder(this.font, text, posX - font.getStringWidth(text) / 2 - 2, posY + 70, WyHelper.hexToRGB("#FFFFFF").getRGB());
+			text = (this.currentIndex + 1) + " / " + this.allDetails.size();
+		WyHelper.drawStringWithBorder(this.font, text, posX - font.getStringWidth(text) / 2 - 2, posY + 80, WyHelper.hexToRGB("#FFFFFF").getRGB());
 			
 		GlStateManager.disableBlend();
 
@@ -148,7 +161,6 @@ public class JollyRogerCreatorScreen extends Screen
 		int posY = this.height / 2;
 		
 		int listPosY = posY - 85;
-	
 		
 		NoTextureButton baseButton = new NoTextureButton(posX + 5, listPosY, 128, 16, "Base", this::selectButton);
 		this.addButton(baseButton);
@@ -167,9 +179,7 @@ public class JollyRogerCreatorScreen extends Screen
 		
 		posX = this.width / 2;
 		
-		TexturedIconButton nextBaseTexture = new TexturedIconButton(ModResources.BRIGHT_WOOD_ARROW_RIGHT, posX + 40, posY - 105, 16, 25, "", (button) -> {
-			
-		});
+		TexturedIconButton nextBaseTexture = new TexturedIconButton(ModResources.BRIGHT_WOOD_ARROW_RIGHT, posX + 40, posY - 105, 16, 25, "", this::nextElement);
 		nextBaseTexture = nextBaseTexture.setTextureInfo(posX + 38, posY - 125, 16, 64);
 		this.addButton(nextBaseTexture);	
 		
@@ -181,6 +191,49 @@ public class JollyRogerCreatorScreen extends Screen
 		*/
 	}
 	
+	public void nextElement(Button btn)
+	{
+		if(this.selectedElement == null)
+			return;
+
+		if(this.layerType == LayerType.BASE)
+		{
+			this.currentIndex = this.findIndex(this.allBases, this.player);					
+			this.currentIndex += 1;
+			
+			if(this.currentIndex == this.allBases.size())
+				this.currentIndex = 0;
+			else if(this.currentIndex == 0)
+				this.currentIndex = this.allBases.size() - 1;
+	
+			this.selectedElement.setTexture(this.allBases.get(this.currentIndex).get().getTexture());
+		}
+		else if(this.layerType == LayerType.BACKGROUND)
+		{
+			this.currentIndex = this.findIndex(this.allBackgrounds, this.player);	
+			this.currentIndex += 1;
+
+			if(this.currentIndex == this.allBackgrounds.size())
+				this.currentIndex = 0;
+			else if(this.currentIndex == 0)
+				this.currentIndex = this.allBackgrounds.size() - 1;
+	
+			this.selectedElement.setTexture(this.allBackgrounds.get(this.currentIndex).get().getTexture());
+		}
+		else if(this.layerType == LayerType.DETAIL)
+		{
+			this.currentIndex = this.findIndex(this.allDetails, this.player);	
+			this.currentIndex += 1;
+
+			if(this.currentIndex == this.allDetails.size())
+				this.currentIndex = 0;
+			else if(this.currentIndex == 0)
+				this.currentIndex = this.allDetails.size() - 1;
+	
+			this.selectedElement.setTexture(this.allDetails.get(this.currentIndex).get().getTexture());
+		}
+	}
+	
 	public void selectButton(Button btn)
 	{
 		if(!(btn instanceof NoTextureButton))
@@ -190,33 +243,36 @@ public class JollyRogerCreatorScreen extends Screen
 			((NoTextureButton) this.selectedButton).select(); 
 		this.selectedButton = btn;
 		((NoTextureButton) btn).select();
-		
+				
 		if(this.buttons.get(0) == btn)
 		{
+			this.selectedElement = this.jollyRoger.getBase();
+			this.currentIndex = this.findIndex(this.getListFromType(LayerType.BASE), this.player);
 			this.layerType = LayerType.BASE;
-			this.layerIndex = 0;
 			return;
 		}
 		
+		int j = 0;
 		for(int i = 1; i < this.jollyRoger.getBackgrounds().length + 1; i++)
 		{
-			int j = 0;
 			if(this.buttons.get(i) == btn)
 			{
+				this.selectedElement = this.jollyRoger.getBackgrounds()[j];
+				this.currentIndex = this.findIndex(this.getListFromType(LayerType.BACKGROUND), this.player);
 				this.layerType = LayerType.BACKGROUND;
-				this.layerIndex = j;
 				return;
 			}
 			j++;
 		}
 			
+		j = 0;
 		for(int i = this.jollyRoger.getBackgrounds().length + 1; i < this.jollyRoger.getDetails().length + this.jollyRoger.getBackgrounds().length + 1; i++)
 		{
-			int j = 0;
 			if(this.buttons.get(i) == btn)
 			{
+				this.selectedElement = this.jollyRoger.getDetails()[j];
+				this.currentIndex = this.findIndex(this.getListFromType(LayerType.DETAIL), this.player);
 				this.layerType = LayerType.DETAIL;
-				this.layerIndex = j;
 				return;
 			}
 			j++;
@@ -228,9 +284,37 @@ public class JollyRogerCreatorScreen extends Screen
 		return true;
 	}
 	
-	public int getTotalElementsForType(LayerType type)
+	private int findIndex(List<RegistryObject<JollyRogerElement>> elements, PlayerEntity player)
 	{
-		return ModJollyRogers.JOLLY_ROGER_ELEMENTS.getEntries().stream().filter(reg -> reg.get().getLayerType() == type).collect(Collectors.toList()).size();
+		if(this.selectedElement == null || this.selectedElement.getTexture() == null)
+			return 0;
+		
+		for(int i = 0; i < elements.size(); i++)
+		{
+			if(elements.get(i).get().getTexture().toString().equalsIgnoreCase(this.selectedElement.getTexture().toString()))
+			{
+				return i;
+			}
+		}
+		
+		return 0;
+	}
+	
+	public List<RegistryObject<JollyRogerElement>> getListFromType(LayerType type)
+	{
+		if(type == LayerType.BASE)
+			return this.allBases;
+		else if(type == LayerType.BACKGROUND)
+			return this.allBackgrounds;
+		else if(type == LayerType.DETAIL)
+			return this.allDetails;
+		
+		return this.allBases;
+	}
+	
+	public List<RegistryObject<JollyRogerElement>> getTotalElementsForType(PlayerEntity player, LayerType type)
+	{
+		return this.allElements.stream().filter(reg -> reg.get().getLayerType() == type && reg.get().canUse(player)).collect(Collectors.toList());
 	}
 	
     public static void open() 
