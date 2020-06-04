261d106ffa3e8943f0a93bf1fc8b23d0423c0ca0
Wynd <wyndmaster@gmail.com>
2020-06-04 11:51:19 +0300

    Pirate/Marine NPCs will attack each other more than the players now

--- src/main/java/xyz/pixelatedw/mineminenomi/entities/mobs/bandits/GenericBanditEntity.java
+++ src/main/java/xyz/pixelatedw/mineminenomi/entities/mobs/bandits/GenericBanditEntity.java
@@ -37,10 +37,10 @@ public class GenericBanditEntity extends GenericNewEntity
 		this.goalSelector.addGoal(5, new LookAtGoal(this, PlayerEntity.class, 8.0F));
 		this.goalSelector.addGoal(5, new LookRandomlyGoal(this));
 
-		this.targetSelector.addGoal(1, new NearestAttackableTargetGoal<>(this, PlayerEntity.class, true));
 		this.targetSelector.addGoal(1, new NearestAttackableTargetGoal<>(this, GenericPirateEntity.class, true));
 		this.targetSelector.addGoal(1, new NearestAttackableTargetGoal<>(this, GenericMarineEntity.class, true));
-		this.targetSelector.addGoal(2, new HurtByTargetGoal(this));
+		this.targetSelector.addGoal(2, new NearestAttackableTargetGoal<>(this, PlayerEntity.class, true));
+		this.targetSelector.addGoal(3, new HurtByTargetGoal(this));
 	}
 	
 	@Override
