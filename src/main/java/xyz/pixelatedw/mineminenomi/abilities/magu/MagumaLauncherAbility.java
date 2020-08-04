dd2d226acd32d774681509230c41f19b4d74da1f
Justin <allard.jjustin@gmail.com>
2020-08-04 12:14:07 +0200

    Maguma launcher now grief land and water

--- src/main/java/xyz/pixelatedw/mineminenomi/abilities/magu/MagumaLauncherAbility.java
+++ src/main/java/xyz/pixelatedw/mineminenomi/abilities/magu/MagumaLauncherAbility.java
@@ -1,12 +1,15 @@
 package xyz.pixelatedw.mineminenomi.abilities.magu;
 
+import net.minecraft.block.Blocks;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.network.play.server.SEntityVelocityPacket;
 import net.minecraft.util.DamageSource;
 import net.minecraft.util.math.Vec3d;
+import xyz.pixelatedw.mineminenomi.api.helpers.AbilityHelper;
 import xyz.pixelatedw.mineminenomi.api.helpers.CrewHelper;
+import xyz.pixelatedw.mineminenomi.api.protection.BlockProtectionRule;
 import xyz.pixelatedw.mineminenomi.particles.effects.ParticleEffect;
 import xyz.pixelatedw.mineminenomi.particles.effects.magu.MagumaLauncherParticleEffect;
 import xyz.pixelatedw.wypi.APIConfig.AbilityCategory;
@@ -22,6 +25,18 @@ public class MagumaLauncherAbility extends ChargeableAbility
 
 	private static final ParticleEffect PARTICLES = new MagumaLauncherParticleEffect();
 
+	private boolean HasLanded;
+	private boolean JustLaunched;
+
+	private BlockProtectionRule GROUND_GRIEF_RULE = new BlockProtectionRule()
+			.addApprovedBlocks(Blocks.GRASS_BLOCK)
+			.addApprovedBlocks(Blocks.SAND)
+			.addApprovedBlocks(Blocks.GRAVEL)
+			.addApprovedBlocks(Blocks.STONE)
+			.addApprovedBlocks(Blocks.DIRT);
+	private BlockProtectionRule WATER_GRIEF_RULE = new BlockProtectionRule()
+			.addApprovedBlocks(Blocks.WATER);
+
 	public MagumaLauncherAbility()
 	{
 		super("Maguma Launcher", AbilityCategory.DEVIL_FRUIT);
@@ -32,6 +47,7 @@ public class MagumaLauncherAbility extends ChargeableAbility
 		this.onStartChargingEvent = this::onStartChargingEvent;
 		this.onEndChargingEvent = this::onEndChargingEvent;
 		this.duringCooldownEvent = this::duringCooldown;
+		this.onEndCooldownEvent = this::onEndCooldownEvent;
 	}
 	
 	private boolean onStartChargingEvent(PlayerEntity player)
@@ -45,11 +61,34 @@ public class MagumaLauncherAbility extends ChargeableAbility
 		player.setMotion(speed.x, 2.0, speed.z);
 
 		((ServerPlayerEntity)player).connection.sendPacket(new SEntityVelocityPacket(player));
+		JustLaunched = true;
+		HasLanded = true;
+		return true;
+	}
+
+	private boolean onEndCooldownEvent(PlayerEntity player)
+	{
+		JustLaunched = false;
+		HasLanded = true;
 		return true;
 	}
 	
 	private void duringCooldown(PlayerEntity player, int cooldownTimer)
 	{
+		if(JustLaunched && !player.onGround)
+		{
+			HasLanded = false;
+			JustLaunched = false;
+		}
+
+		if(!player.onGround && !HasLanded)
+		{
+			AbilityHelper.createFilledSphere(player.world, (int) player.posX, (int) player.posY, (int) player.posZ, 2, Blocks.OBSIDIAN, WATER_GRIEF_RULE);
+			AbilityHelper.createFilledSphere(player.world, (int) player.posX, (int) player.posY, (int) player.posZ, 2, Blocks.MAGMA_BLOCK, GROUND_GRIEF_RULE);
+		}
+		else
+			HasLanded = true;
+
 		if ((cooldownTimer / 20) > (this.maxCooldown / 20) - 3)
 		{
 			if(cooldownTimer % 2 == 0)
