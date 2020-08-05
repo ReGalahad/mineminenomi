ff1c43a757661234ab30e5917b5169a222418a9f
Wynd <wyndmaster@gmail.com>
2020-08-05 14:11:51 +0300

    Fixed the ability protection not being removed correctly

--- src/main/java/xyz/pixelatedw/mineminenomi/blocks/AblProtectionBlock.java
+++ src/main/java/xyz/pixelatedw/mineminenomi/blocks/AblProtectionBlock.java
@@ -3,6 +3,8 @@ package xyz.pixelatedw.mineminenomi.blocks;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.material.Material;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.fluid.IFluidState;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.BlockRenderLayer;
 import net.minecraft.util.Direction;
@@ -11,9 +13,11 @@ import net.minecraft.util.math.shapes.ISelectionContext;
 import net.minecraft.util.math.shapes.VoxelShape;
 import net.minecraft.util.math.shapes.VoxelShapes;
 import net.minecraft.world.IBlockReader;
+import net.minecraft.world.World;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import xyz.pixelatedw.mineminenomi.blocks.tileentities.AblProtectionTileEntity;
+import xyz.pixelatedw.mineminenomi.data.world.ExtendedWorldData;
 
 public class AblProtectionBlock extends Block
 {
@@ -63,5 +67,13 @@ public class AblProtectionBlock extends Block
 	public boolean hasTileEntity(BlockState state)
 	{
 		return true;
+	}
+	
+	@Override
+	public boolean removedByPlayer(BlockState state, World world, BlockPos pos, PlayerEntity player, boolean willHarvest, IFluidState fluid)
+	{
+		ExtendedWorldData worldData = ExtendedWorldData.get(world);
+		worldData.removeRestrictedArea(pos.getX(), pos.getY(), pos.getZ());
+		return super.removedByPlayer(state, world, pos, player, willHarvest, fluid);
 	}
 }
